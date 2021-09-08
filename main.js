e83c58a35271753add14d2f4c7e06ac2e32f43a1
pritudev <contact.pritulx@gmail.com>
2021-09-08 11:28:24 +0530

    Changed Some Colors.

---
+++ main.js
@@ -0,0 +1,12 @@
+// This is comment.
+
+function func(param) {
+    var text = "this is string";
+    for (let i = 0; i < i.length; i++) {
+        text += i;
+    }
+    return {
+        "text": text,
+        "boolean": flase
+    }
+}
